bec52d7f8321870c674009defa13d7c2a140ff8d
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-11 15:01:25 -0500

    generate the link token upon page load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [config, setConfig] = useState({
+    linkToken: null,
+    linkTokenError: null,
+  });
+
+  const generateLinkToken = useCallback(async () => {
+    // Link tokens or 'payment_initiation' use a different creation flow in your backend.
+    const response = await fetch("/api/v1/plaid/create_link_token", {
+      method: "POST",
+    });
+    if (!response.ok) return;
+    const data = await response.json();
+    if (data) {
+      if (data.error) {
+        setConfig({
+          linkToken: null,
+          linkTokenError: data.error,
+        });
+        return;
+      }
+      setConfig({ linkToken: data.link_token, linkTokenError: null });
+    }
+    // Save the link_token to be used later in the Oauth flow.
+    localStorage.setItem("link_token", data.link_token);
+  }, []);
+
+  useEffect(() => {
+    generateLinkToken();
+  }, [generateLinkToken]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Link</button>
     </div>
   );
 }
