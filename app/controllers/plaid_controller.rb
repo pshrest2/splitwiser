6f961dd874f48d033015c43b2101e7c96408c73e
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-11 14:26:06 -0500

    remove unnecessary require and include call and return error json on rescue

--- app/controllers/plaid_controller.rb
+++ app/controllers/plaid_controller.rb
@@ -1,13 +1,11 @@
-require 'plaid'
-
 class PlaidController < ApplicationController
-  include PlaidApi
-
   # GET /plaid/link_token
   def link_token
     link_token_api = PlaidApi::CreateLinkToken.new
     link_token = link_token_api.call
 
     render json: {token: link_token}, status: :ok
+  rescue => e
+    render json: {error: "Something went wrong"}, status: :unprocessable_entity
   end
 end
