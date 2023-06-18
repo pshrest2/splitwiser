17f3322c38b7d51ef2c6fca5118d940fbfd062ed
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-18 17:12:46 -0500

    use PLAID_CLIENT_NAME env variable

--- lib/plaid_api/create_link_token.rb
+++ lib/plaid_api/create_link_token.rb
@@ -4,7 +4,7 @@ module PlaidApi
       # Create a link_token for the given user
       request = Plaid::LinkTokenCreateRequest.new({
         user: { client_user_id: "1" },
-        client_name: ENV["CLIENT_NAME"],
+        client_name: ENV["PLAID_CLIENT_NAME"],
         products: [ENV["PLAID_PRODUCTS"]],
         country_codes: [ENV["PLAID_COUNTRY_CODES"]],
         language: "en",
