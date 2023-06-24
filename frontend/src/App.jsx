d7c599fe13b06ef04c3fd2b9ff3bdf487c834b23
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-24 09:35:12 -0500

    no need for this crap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,23 +7,9 @@ import HomePage from "./pages/HomePage";
 import "./App.scss";
 import RequireAuth from "./components/RequireAuth";
 import AccountPage from "./pages/AccountPage";
-import { useDispatch } from "react-redux";
-import { useCallback, useEffect } from "react";
 
 function App() {
-  const { isLoading, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const dispatch = useDispatch();
-  const accessToken = localStorage.getItem("accessToken");
-
-  const saveTokenToRedux = useCallback(async () => {
-    if (isAuthenticated && !accessToken) {
-      localStorage.setItem("accessToken", await getAccessTokenSilently());
-    }
-  }, [accessToken, getAccessTokenSilently, isAuthenticated]);
-
-  useEffect(() => {
-    saveTokenToRedux();
-  }, [isAuthenticated, getAccessTokenSilently, dispatch, saveTokenToRedux]);
+  const { isLoading } = useAuth0();
 
   if (isLoading) return <div>Loading...</div>;
 
