253ce9ef9023265ebd94b09c6c3903a0d80d4619
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-24 11:40:15 -0500

    fix the useAccessToken hook

--- frontend/src/hooks/useAccessToken.js
+++ frontend/src/hooks/useAccessToken.js
@@ -1,19 +1,27 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useCallback, useEffect, useState } from "react";
 
 const useAccessToken = () => {
-  const { getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [accessToken, setAccessToken] = useState(null);
 
-  const setAccessToken = async () => {
-    const accessToken = await getAccessTokenSilently();
-    localStorage.setItem("splitwiser", JSON.stringify({ accessToken }));
-    return { accessToken };
-  };
+  const loadAccessToken = useCallback(async () => {
+    const splitwiserObject = localStorage.getItem("splitwiser");
+    let currAccessToken = JSON.parse(splitwiserObject || "{}").accessToken;
 
-  const splitwiserObject = localStorage.getItem("splitwiser");
-  if (!splitwiserObject) return setAccessToken();
+    if (!currAccessToken) {
+      currAccessToken = await getAccessTokenSilently();
+      localStorage.setItem(
+        "splitwiser",
+        JSON.stringify({ accessToken: currAccessToken })
+      );
+    }
+    setAccessToken(currAccessToken);
+  }, [getAccessTokenSilently]);
 
-  let accessToken = JSON.parse(splitwiserObject).accessToken;
-  if (!accessToken) return setAccessToken();
+  useEffect(() => {
+    if (isAuthenticated) loadAccessToken();
+  }, [loadAccessToken, isAuthenticated]);
 
   return { accessToken };
 };
