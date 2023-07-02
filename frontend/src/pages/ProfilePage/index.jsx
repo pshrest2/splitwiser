6fe6f8e2fa63b5e15e6a4f0f0453b9722c96e022
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-02 15:10:35 -0500

    move Profiles to its own component

--- frontend/src/pages/ProfilePage/index.jsx
+++ frontend/src/pages/ProfilePage/index.jsx
@@ -1,95 +1,12 @@
-import { useCallback, useState } from "react";
-import { Button, Form, Image } from "react-bootstrap";
-import { toast } from "react-toastify";
-import { useAuth0 } from "@auth0/auth0-react";
-
 import BackgroundContainer from "../../components/BackgroundContainer";
-import { updateUser } from "../../api/apiCalls";
-import useAccessToken from "../../hooks/useAccessToken";
+import Profile from "./Profile";
+import Accounts from "./Accounts";
 
 const ProfilePage = () => {
-  const { user: auth0_user } = useAuth0();
-  const accessToken = useAccessToken();
-  const [editUser, setEditUser] = useState(false);
-  const [user, setUser] = useState(auth0_user);
-
-  const handleFormSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
-      const result = await updateUser(user.sub, user, accessToken);
-      if (result.error)
-        toast.error("There was an error updating user. Please try again later");
-      else {
-        toast.success(result.message);
-        setEditUser(false);
-      }
-    },
-    [accessToken, user]
-  );
-
-  const handleFieldChange = (e) => {
-    setUser((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
   return (
     <BackgroundContainer>
-      {user && (
-        <div className="d-flex">
-          <div className="mr-3">
-            <Image src={user.picture} alt="profile-picture" />
-          </div>
-          <div className="d-flex flex-column">
-            {!editUser ? (
-              <div className="w-100 mx-5">
-                <h1>{user.name}</h1>
-                <h6>{user.email}</h6>
-              </div>
-            ) : (
-              <Form className="w-100 mx-5" onSubmit={handleFormSubmit}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    value={user.name}
-                    onChange={handleFieldChange}
-                    placeholder="Enter your full name"
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    disabled
-                  />
-                </Form.Group>
-                <div className="d-flex justify-content-end">
-                  <Button
-                    variant="outline-secondary"
-                    className="mx-1"
-                    onClick={() => setEditUser(false)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button type="submit">Save</Button>
-                </div>
-              </Form>
-            )}
-          </div>
-          {!editUser && (
-            <div>
-              <Button variant="link" onClick={() => setEditUser(true)}>
-                Edit
-              </Button>
-            </div>
-          )}
-        </div>
-      )}
+      <Profile />
+      <Accounts />
     </BackgroundContainer>
   );
 };
