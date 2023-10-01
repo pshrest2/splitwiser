f2ce2f5a57579ca85d1da63b806f4068459bb8f2
pshrestha <pranaya.shrestha2@gmail.com>
2023-10-01 09:48:16 -0500

    test: added test for rest of the model

--- spec/models/user_expense_spec.rb
+++ spec/models/user_expense_spec.rb
@@ -22,5 +22,6 @@
 require 'rails_helper'
 
 RSpec.describe UserExpense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:expense) }
 end
