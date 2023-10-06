21c5b18da9da45309bd1c4c9623d625d586b5e99
pshrestha <pranaya.shrestha2@gmail.com>
2023-10-05 22:40:40 -0500

    feat: added endpoint to create/update user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,24 +9,46 @@ module Api
         @api ||= Auth0Api::User.new
       end
 
-      # GET /users/:id
+      # GET /users/:sub
       def show
-        user = @api.fetch(params[:id])
+        user = User.find_by(params[:sub])
         render json: user, status: :ok
       end
 
       # PATCH /users/:id
       def update
-        @api.update(params[:id], user_params)
+        @api.update(params[:id], user_edit_params)
 
         render_success('User updated successfully')
       rescue StandardError => _e
         render_error
       end
 
+      # POST /users
+      def create
+        user = User.find_by(sub: user_params[:sub]) || User.find_by(email: user_params[:email])
+
+        if user
+          render_success('User already registered')
+          return
+        end
+
+        user = User.new(user_params)
+
+        if user.save
+          render_success('User created successfully')
+        else
+          render_error('Could not create user')
+        end
+      end
+
       private
 
       def user_params
+        params.require(:user).permit(:name, :email, :email_verified, :sub, :picture)
+      end
+
+      def user_edit_params
         params.require(:user).permit(:name, :picture)
       end
     end
